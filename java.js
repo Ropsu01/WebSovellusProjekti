b4e517ac1df2a4730fc56f1e531aacac0fb833eb
Roope <roope.nahkala@gmail.com>
2023-09-08 12:37:35 +0300

    testing

---
+++ java.js
@@ -0,0 +1,19 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    // Hide all tab content
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Deactivate all tab links
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    // Show the selected tab content and activate the tab link
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+}
