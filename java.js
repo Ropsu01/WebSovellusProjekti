a8e759ecb7b50514a4dfebefbd2125c4a3fe6f27
Roope <roope.nahkala@gmail.com>
2023-10-03 20:02:21 +0300

    update

--- java.js
+++ java.js
@@ -46,5 +46,43 @@ function openEpisodeTab(tabName) {
 }
 
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("imageSlides");
+  var thumbnails = document.getElementsByClassName("thumbnail");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].className = thumbnails[i].className.replace(" active", "");
+}
+  slides[slideIndex-1].style.display = "block";
+  thumbnails[slideIndex-1].className += " active";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
 
 
